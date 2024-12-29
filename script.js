0d340ffd64f10cf06ffe4ca696bd48b168d328f4
AyaanSucksAtCoding <splytplayz@gmail.com>
2024-12-29 16:31:50 +0500

    dark mode intializated

---
+++ script.js
@@ -0,0 +1,8 @@
+const themeToggle = document.querySelector('.theme-controller');
+themeToggle.addEventListener('change', function() {
+  if (this.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
